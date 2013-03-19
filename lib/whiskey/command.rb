078744833b784c247a5909de5798cc5344aeb957
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-03-19 03:56:45 -0700

    Fleshing out the build action into it's own class

--- lib/whiskey/command.rb
+++ lib/whiskey/command.rb
@@ -1,9 +1,13 @@
 require "thor"
 require "whiskey"
+require "active_support/core_ext"
+require_relative "command/build"
 
 module Whiskey
   class Command < Thor
     include Thor::Actions
+
+    attr_accessor :name
 
     def self.source_root
       File.join(File.dirname(__FILE__), "command", "templates")
@@ -11,41 +15,10 @@ module Whiskey
 
     desc "build NAME", "Builds a new whiskey project from the scaffold"
     def build(name)
-      empty_directory(name)
-      self.destination_root = name
-      template("Gemfile")
-      template("gitignore", ".gitignore")
-      template("LICENSE.txt")
-      template("Thorfile")
-      template("server.rb")
-      directory("script")
+      Build.new(self, name).call
+    end
 
-      empty_directory("lib")
-      inside("lib") do
-        template("example.rb", "#{name}.rb")
-        template("models.rb")
-        empty_directory("models")
-        inside("models") do
-          template("account.rb")
-          template("place.rb")
-        end
       end
-
-      empty_directory("static")
-      inside("static") do
-        template("welcome.txt")
-      end
-
-      empty_directory("db")
-      inside("db") do
-        # if persistence == activerecord
-        empty_directory("migrations")
-        template("seed.rb")
-      end
-
-      empty_directory("log")
-      empty_directory("tmp")
-      empty_directory("doc")
     end
   end
 end
---
+++ lib/whiskey/command/build.rb
@@ -0,0 +1,73 @@
+module Whiskey
+  class Command < Thor
+    class Build
+      attr_accessor :command
+
+      def initialize(command, name)
+        self.command = command
+        self.name = name
+      end
+
+      def call
+        create_root
+        setup_root
+        setup_server
+        setup_static
+        setup_db
+      end
+
+      private
+
+      def create_root
+        empty_directory(name)
+        command.destination_root = name
+      end
+
+      def setup_root
+        template("Gemfile")
+        template("gitignore", ".gitignore")
+        template("LICENSE.txt")
+        template("Thorfile")
+        template("server.rb")
+        empty_directories("script", "log", "tmp", "doc")
+      end
+
+      def empty_directories(*directories)
+        directories.each { |directory| empty_directory directory }
+      end
+
+      def setup_server
+        inside("server") do
+          template("example.rb", "#{name}.rb")
+          template("models.rb")
+          setup_models
+        end
+      end
+
+      def setup_models
+        inside("models") do
+          template("account.rb")
+          template("place.rb")
+        end
+      end
+
+      def setup_static
+        inside("static") do
+          template("welcome.txt")
+        end
+      end
+
+      def setup_db
+        inside("db") do
+          # if persistence == activerecord
+          empty_directory("migrations")
+          template("seed.rb")
+        end
+      end
+
+      def method_missing(method, *arguments, &block)
+        command.send(method, *arguments, &block)
+      end
+    end
+  end
+end
