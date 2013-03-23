6968387bafb5a0ba7980c742c388e5a1ddae5394
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-03-22 21:07:20 -0700

    We don't need these methods on this class

--- lib/whiskey/server/connection.rb
+++ lib/whiskey/server/connection.rb
@@ -15,26 +15,10 @@ module Whiskey
         "#{ip}:#{port}"
       end
 
-      def connected
-        puts "#{id} has connect!"
-      end
-
-      def disconnected
-        puts "#{id} has disconnected!"
-      end
-
       private
-
-      def family
-        peeraddr[0]
-      end
 
       def port
         peeraddr[1]
-      end
-
-      def host
-        peeraddr[2]
       end
 
       def ip
