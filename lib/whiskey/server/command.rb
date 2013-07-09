0467c623a892af55013012c0e14dcc6e05116861
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-09 13:00:09 -0700

    We want a module we can inherit for server commands

---
+++ lib/whiskey/server/command.rb
@@ -0,0 +1,13 @@
+module Whiskey
+  class Server
+    module Command
+      def initialize(*args)
+        raise "Not yet implemented"
+      end
+
+      def call
+        raise "Not yet implemented"
+      end
+    end
+  end
+end
