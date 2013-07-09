20e607aba3f49670498eb64df57aa95f0503499e
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-09 13:00:43 -0700

    The interpretor needs to handle doing things and errors

--- lib/whiskey/server/interpretor.rb
+++ lib/whiskey/server/interpretor.rb
@@ -1,20 +1,39 @@
 module Whiskey
   class Server
     class Interpretor
-      attr_reader :input
+      attr_reader :response
 
       def initialize(input)
-        @input = AltStruct.new(input)
+        @instructions = input
+        @repsonse = if has_scope? then call else not_found end
       end
 
       def call
-        case input.resource.to_s
-          when "world" then World.new
+        if command.valid?
+          @response = { status: 200 }
+        else
+          @response = Error.new(:unprocessable_entity).to_hash
         end
       end
 
-      def response
-        call
+      def instructions
+        AltStruct.new(@instructions)
+      end
+
+      private
+
+      def command
+        case instructions.scope.to_s
+          when "world" then ::Command::World
+        end.new(instructions.command.to_s)
+      end
+
+      def has_scope?
+        @instructions.has_key?("scope")
+      end
+
+      def not_found
+        Error.new(:not_found).to_hash
       end
     end
   end
