a548c5d819249ff809726e13ec690d2de3ed558b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-09 12:59:22 -0700

    Moving static welcome text to a seeded database entry

--- lib/whiskey/command/templates/db/seed.rb
+++ lib/whiskey/command/templates/db/seed.rb
@@ -1,1 +1,15 @@
 # This is where you'll place any logic that boots up your instance from scratch
+
+introduction = Document.create do |doc|
+  doc.title = "Introduction"
+  doc.body = """
+    Welcome to <%= name.camelize %> a new MUTE!
+
+    To make an account here simply type:
+
+      account create a_suitable_username a_suitable_password
+
+    To login to your account type:
+
+      account access a_suitable_username a_suitable_password
+  """
