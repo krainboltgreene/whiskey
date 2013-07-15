693da2cb3e806fe817fc7af42dbfb9f8816025b0
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-15 02:05:42 -0700

    Just in case someone sends something other than PULL/PUSH

--- lib/whiskey/server/error.rb
+++ lib/whiskey/server/error.rb
@@ -49,6 +49,14 @@ module Whiskey
           status: 500
         }
       end
+
+      def method_not_allowed
+        {
+          title: "Method Not Allowed",
+          body: "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI. The response MUST include an Allow header containing a list of valid methods for the requested resource.",
+          status: 405
+        }
+      end
     end
   end
 end
