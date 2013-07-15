2d55e60853487817abf90f92a7a2188136672aaa
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-15 02:07:50 -0700

    Routers should determine what module/class gets called

--- lib/whiskey/server/router.rb
+++ lib/whiskey/server/router.rb
@@ -2,7 +2,39 @@ module Whiskey
   class Server
     class Router
       def initialize(control, verb, parameters = {})
-        raise "Not yet implemented"
+        @parameters = parameters
+        @action = verb.upcase
+        @control = control
+      end
+
+      def parameters
+        OpenStruct.new(@parameters)
+      end
+
+      def to_hash
+        if control.safe_constantize && action.safe_constantize
+          control_action.safe_constantize.new(parameters).to_hash
+        else
+          Error.new(:not_found).to_hash
+      end
+
+      def control
+        "Controls::#{@control.classify}Control"
+      end
+
+      def action
+        case
+          when @action == "PULL" then "ListAction"
+          when @action == "PUSH" then "CreateAction"
+          when @action == "PULL" && parameters.id then "ShowAction"
+          when @action == "PUSH" && parameters.id then "UpdateAction"
+        end
+      end
+
+      private
+
+      def control_action
+        "#{control}::#{action}"
       end
     end
   end
