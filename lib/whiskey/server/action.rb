4c7b4d44cc40c70cf97942ad3962e0401e1d5b9b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-07-16 04:08:25 -0700

    We need to access the class to verb as a singleton method

--- lib/whiskey/server/action.rb
+++ lib/whiskey/server/action.rb
@@ -1,6 +1,14 @@
 module Whiskey
   class Server
     class Action
+      def self.to_verb
+        case name.demodulize
+          when "ListAction" then "PULL"
+          when "CreateAction" then "PUSH"
+          when "ShowAction" then "PULL"
+          when "UpdateAction" then "PUSH"
+        end
+      end
 
       def initialize(*args)
         raise "Not yet implemented"
@@ -8,15 +16,6 @@ module Whiskey
 
       def to_hash
         raise "Not yet implemented"
-      end
-
-      def to_verb
-        case name
-          when "ListAction" then "PULL"
-          when "CreateAction" then "PUSH"
-          when "ShowAction" then "PULL"
-          when "UpdateAction" then "PUSH"
-        end
       end
     end
   end
