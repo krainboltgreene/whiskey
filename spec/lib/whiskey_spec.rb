9b2325587ef1d10482003b4df3c1a08f60202ef9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-11-24 04:06:48 -0800

    We want some pending tests for logger to write later

--- spec/lib/whiskey_spec.rb
+++ spec/lib/whiskey_spec.rb
@@ -1,5 +1,8 @@
 require "spec_helper"
 
 describe Whiskey do
-
+  describe ".logger" do
+    it "has a default log level of DEBUG in development mode"
+    it "has a default log level of INFO in production mode"
+  end
 end
